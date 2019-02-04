4c0691a6755a1ac84b3508a8f7a9d17b3f0478a0
Eduardo Verdeja <eaverdeja@gmail.com>
2019-02-03 22:24:40 -0200

    Adicionando os source maps

--- es6-curso/15-js-modules/webpack.config.js
+++ es6-curso/15-js-modules/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false },
-      // sourceMap: true,
+      sourceMap: true,
     }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(nodeENV) },
