d3897587e2fabbe4635d0474fe51038b0ab8a8cf
Eduardo Verdeja <eaverdeja@gmail.com>
2019-02-18 21:24:35 -0300

    Introdução aos hooks do mocha

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,39 +1,25 @@
 describe('Index', () => {
-  describe('Bools', () => {
-    context('only() on it()', () => {
-      it.only('does something special', () => false)
-    })
-
-    context.only('only() on context()', () => {
-      it('does something very special', () => true)
-      it('does something more special', () => !false)
-    })
+  before(() => {
+    console.log('before')
   })
 
-  /**
-   * The whole describe() block is skipped because
-   * of the only() calls. Removing the describe(Bools)
-   * block would call Case 1, but we would still have
-   * 2 pending tests (one success and one failure)
-   *
-   * O bloco describe() abaixo é ignorado
-   * por conta das chamadas à only(). Remover o bloco
-   * describe(Bools) chamaria o Case 1, mas ainda teríamos
-   * dois testes pendentes (um sucesso e uma falha)
-   */
-  describe('Logs', () => {
-    context('Case 1', () => {
-      it('does something lame', () => {
-        console.log('something lame')
-      })
-      it.skip('would fail!', () => {
-        throw new Error('BOOM!')
-      })
-    })
-    context.skip('skip() on context()', () => {
-      it('does something very lame', () => {
-        console.log(`${!!false}`)
-      })
-    })
+  after(() => {
+    console.log('after')
+  })
+
+  beforeEach(() => {
+    console.log('\tbeforeEach')
+  })
+
+  afterEach(() => {
+    console.log('\tafterEach')
+  })
+
+  it('is the first test', () => {
+    console.log('\t  first test!')
+  })
+
+  it('is the second test', () => {
+    console.log('\t  second test!')
   })
 })
