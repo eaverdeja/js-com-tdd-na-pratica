5dbb2d9e0dddba4b6244d3dbb6dc66a5febeccc4
Eduardo Verdeja <eaverdeja@gmail.com>
2019-02-18 22:06:51 -0300

    Adicionando as assertivas do chai

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,3 +1,5 @@
+const expect = require('chai').expect
+
 describe('Index', () => {
   let arr
 
@@ -5,17 +7,31 @@ describe('Index', () => {
     arr = [1, 2, 3]
   })
 
+  // Smoke test
+  it('should be an array', () => {
+    expect(arr).to.be.an('array')
+  })
+
   it('should have a size of 4 when we push a value into the array', () => {
     arr.push(4)
-    console.log(arr.length)
+    expect(arr).to.have.a.lengthOf(4)
   })
 
   it('should retrieve the value 3 from the array if we pop it', () => {
-    console.log(arr.pop() === 3)
+    expect(arr.pop()).to.equal(3)
+    expect(arr).to.not.include(3)
   })
 
   it('should have a size of 2 when we pop a value from the array', () => {
     arr.pop()
-    console.log(arr.length)
+    expect(arr).to.have.a.lengthOf(2)
+  })
+
+  it('should still find 1 and 2 if we pop the array', () => {
+    arr.pop()
+    const hasOne = arr.includes(1)
+    const hasTwo = arr.includes(2)
+    expect(hasOne).to.be.true
+    expect(hasTwo).to.be.true
   })
 })
