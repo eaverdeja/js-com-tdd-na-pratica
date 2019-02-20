4e6418ef066ed612943c2fee4e0548da16a9862d
Eduardo Verdeja <eaverdeja@gmail.com>
2019-02-19 21:38:17 -0300

    Parametrizando os testes

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,57 +3,91 @@ const expect = require('chai').expect
 const calc = require('../src/index')
 
 describe('Calc', () => {
+  const calcMethods = [
+    {
+      name: 'sum',
+      tests: [
+        {
+          num1: 2,
+          num2: 2,
+          expect: 4,
+        },
+        {
+          num1: 2,
+          num2: -1,
+          expect: 1,
+        },
+      ],
+    },
+    {
+      name: 'sub',
+      tests: [
+        {
+          num1: 2,
+          num2: 2,
+          expect: 0,
+        },
+        {
+          num1: 2,
+          num2: -1,
+          expect: 3,
+        },
+      ],
+    },
+    {
+      name: 'mult',
+      tests: [
+        {
+          num1: 2,
+          num2: 2,
+          expect: 4,
+        },
+        {
+          num1: 3,
+          num2: 3,
+          expect: 9,
+        },
+      ],
+    },
+    {
+      name: 'div',
+      tests: [
+        {
+          num1: 2,
+          num2: 2,
+          expect: 1,
+        },
+        {
+          num1: 12,
+          num2: 3,
+          expect: 4,
+        },
+      ],
+    },
+  ]
+
   describe('Smoke Tests', () => {
     it("should assert the calc lib's existence", () => {
       expect(calc).to.exist
     })
-
-    const calcMethods = ['sum', 'sub', 'mult', 'div']
     calcMethods.forEach((method) => {
-      it(`should have a ${method} method`, () => {
-        expect(calc[method]).to.exist
-        expect(calc[method]).to.be.a('function')
+      it(`should have a ${method.name} method`, () => {
+        expect(calc[method.name]).to.exist
+        expect(calc[method.name]).to.be.a('function')
       })
     })
   })
 
-  describe('Sum', () => {
-    it('should return 4 when `sum(2,2)`', () => {
-      expect(calc.sum(2, 2)).to.be.equal(4)
-    })
-
-    it('should return 1 when `sum(2,-1)`', () => {
-      expect(calc.sum(2, -1)).to.be.equal(1)
-    })
-  })
-
-  describe('Sub', () => {
-    it('should return 0 when `sub(2,2)`', () => {
-      expect(calc.sub(2, 2)).to.be.equal(0)
-    })
-
-    it('should return 3 when `sub(2,-1)`', () => {
-      expect(calc.sub(2, -1)).to.be.equal(3)
-    })
-  })
-
-  describe('Mult', () => {
-    it('should return 4 when `mult(2,2)`', () => {
-      expect(calc.mult(2, 2)).to.be.equal(4)
-    })
-
-    it('should return 9 when `mult(3,3)`', () => {
-      expect(calc.mult(3, 3)).to.be.equal(9)
-    })
-  })
-
-  describe('Div', () => {
-    it('should return 1 when `div(2,2)`', () => {
-      expect(calc.div(2, 2)).to.be.equal(1)
-    })
-
-    it('should return 4 when `div(12,3)`', () => {
-      expect(calc.div(12, 3)).to.be.equal(4)
+  describe('Calc Methods', () => {
+    calcMethods.forEach((method) => {
+      method.tests.forEach((test) => {
+        it(`should return ${test.expect} when \`${method.name}(${test.num1},${
+          test.num2
+        })\``, () => {
+          const result = calc[method.name](test.num1, test.num2)
+          expect(result).to.be.equal(test.expect)
+        })
+      })
     })
   })
 })
