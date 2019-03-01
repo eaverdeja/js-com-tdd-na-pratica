6bb228e9da2e3792abd89e98493414d21c9066ff
Eduardo Verdeja <eaverdeja@gmail.com>
2019-02-28 21:37:19 -0300

    Preparando o repo para criar o wrapper da API do Spotify

---
+++ fundamentos/src/calculator/index.js
@@ -0,0 +1,6 @@
+export default {
+  sum: (num1, num2) => num1 + num2,
+  sub: (num1, num2) => num1 - num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => (num2 === 0 ? 'Não é possível dividir por zero!' : num1 / num2),
+}
